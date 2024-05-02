901638b89402532d67aede819bc7ea4395bffd27
Özge Odabaş <84701674+odbsozge@users.noreply.github.com>
2024-05-03 01:50:40 +0300

    some changes

--- src/main/java/org/example/magnificentgallery/HelloController.java
+++ src/main/java/org/example/magnificentgallery/HelloController.java
@@ -1,14 +1,28 @@
 package org.example.magnificentgallery;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
+    private Alert alert = new Alert(Alert.AlertType.ERROR);
+ //   @FXML
+//    private Image image = new Image("Screenshot.png");
+    @FXML
+    ImageView img = new ImageView();
+
+
+@FXML
     protected void onHelloButtonClick() {
+
+    //img.setImage(image);
+        alert.show();
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
